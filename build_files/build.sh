367a4d8a8d188de40c74e498e74df3d4ab77ad3e
Young, Gianni <cmancy@tutanota.com>
2025-11-26 12:06:56 -0600

    cleaning

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,13 +3,10 @@
 set -ouex pipefail
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
 dnf5 install -y zsh kitty fish
 
 dnf5 -y copr enable scottames/ghostty
