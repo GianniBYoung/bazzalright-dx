ba1ad733419f4a09a96a0f21240d539c8d70569e
Young, Gianni <cmancy@tutanota.com>
2026-05-17 10:02:06 -0500

    feat: terra-release

--- build_files/01_build.sh
+++ build_files/01_build.sh
@@ -7,6 +7,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf5 -y install --refresh terra-release
+sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/terra.repo
 dnf5 install -y --refresh zsh kitty fish vicinae
 
 # Add Incus Repo
