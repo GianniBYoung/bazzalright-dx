044354e7bd0c6da2e1d069aaf22b07f61bf6e3f9
Young, Gianni <cmancy@tutanota.com>
2026-08-08 17:26:13 -0500

    feat: ghostty sysext installation

--- build_files/03_sysexts.sh
+++ build_files/03_sysexts.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+
+# sysext prereqs
 install -d -m 0755 -o 0 -g 0 "/var/lib/extensions" "/var/lib/extensions.d"
 restorecon -RFv "/var/lib/extensions" "/var/lib/extensions.d"
 
 systemctl enable --now systemd-sysext.service
+
+# ghostty sysext installation
+SYSEXT=ghostty
+FCOS_COMMUNITY_URL=https://extensions.fcos.fr/community
+install -d -m 0755 -o 0 -g 0 "/etc/sysupdate.${SYSEXT}.d"
+restorecon -RFv "/etc/sysupdate.${SYSEXT}.d"
+curl --silent --fail --location "${FCOS_COMMUNITY_URL}/${SYSEXT}.conf" \
+| tee "/etc/sysupdate.${SYSEXT}.d/${SYSEXT}.conf"
+/usr/lib/systemd/systemd-sysupdate update --component $SYSEXT
+
+# # Helper function for fedora-coreos community sysext installation
+# install_sysext() {
+#   SYSEXT="${1}"
+#   URL="https://extensions.fcos.fr/community"
+#   sudo install -d -m 0755 -o 0 -g 0 "/etc/sysupdate.${SYSEXT}.d"
+#   sudo restorecon -RFv "/etc/sysupdate.${SYSEXT}.d"
+#   curl --silent --fail --location "${URL}/${SYSEXT}.conf" \
+#     | sudo tee "/etc/sysupdate.${SYSEXT}.d/${SYSEXT}.conf"
+#   sudo /usr/lib/systemd/systemd-sysupdate update --component "${SYSEXT}"
+# }
